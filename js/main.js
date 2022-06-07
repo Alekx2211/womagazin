728625071279f4f14bdb91ce0bb384baccb06137
Alekx2211 <42638496+Alekx2211@users.noreply.github.com>
2022-06-07 23:22:07 +0300

    sdas

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 var swiper = new Swiper(".shop", {
     pagination: {
         el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+var swiper = new Swiper(".teams", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
 });
